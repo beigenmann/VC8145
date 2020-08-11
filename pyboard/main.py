9084663438d38294ee4816ad893f690d2b7648a2
Benno Eigenmann <beigenmann@gmail.com>
2020-08-11 19:42:39 +0200

    init

--- pyboard/main.py
+++ pyboard/main.py
@@ -1,11 +1,4 @@
 import wifimgr
-from time import sleep
-import machine
-
-try:
-  import usocket as socket
-except:
-  import socket
 
 
 wlan = wifimgr.get_connection()
@@ -13,6 +6,7 @@ if wlan is None:
     print("Could not initialize the network connection.")
     while True:
         pass  # you shall not pass :D
+
 
 # Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
 print("ESP OK")
